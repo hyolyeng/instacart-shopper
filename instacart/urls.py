b62b6228c2d9b5bc3a013c98c4f8a1caa114239e
Diana Jung <hyolyeng@gmail.com>
2017-05-22 20:23:22 -0700

    database migration + website icon

--- instacart/urls.py
+++ instacart/urls.py
@@ -20,6 +20,7 @@ import shoppers.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', shoppers.views.index),
     url(r'^shoppers/index$', shoppers.views.index, name='index'),
     url(r'^shoppers/application_status/(?P<id>\d+)/$', shoppers.views.application_status, name='application_status'),
     url(r'^shoppers/application', shoppers.views.application, name='application'),
